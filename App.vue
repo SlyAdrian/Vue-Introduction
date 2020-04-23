1e107d12a959adf7ae5c309be269227dca3543fb
SlyAdrian <blop.avorad@outlook.fr>
2020-04-23 08:58:00 +0200

    addition of the app

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>  
+    <h1>Clicked {{ timesClicked }} times.</h1>
+    <button @click="iAmClicked()">Click me!</button>
+  </div>
+</template>
+ 
+<script>
+    test1 : 0;
+    export default {
+        
+        data() {
+            return {timesClicked: 0}
+        },
+        methods: {
+            iAmClicked() {
+                this.timesClicked ++ ;
+            }
+        }
+    };
+</script>
